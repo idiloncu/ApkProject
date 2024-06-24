deb9263b1d37117a07f3de71268c2787f41b375b
İdil Öncü <oncuidil@gmail.com>
2024-06-24 18:43:32 +0300

    Mqtt functions added but it has no device connection

---
+++ app/src/main/java/com/example/apkproject/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.apkproject
+
+
+
+const val SERVER_URI = "tcp://deneme-234@broker.mqtt.cool:1883"
+
+
--- app/src/main/java/com/example/apkproject/MainActivity.kt
+++ app/src/main/java/com/example/apkproject/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.apkproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.example.apkproject.databinding.ActivityMainBinding
 import info.mqtt.android.service.MqttAndroidClient
@@ -19,9 +20,13 @@ import javax.security.auth.callback.Callback
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var mqttClient:MqttAndroidClient
+    private lateinit var mqttClient: MqttAndroidClient
     var currentIndex = 0
-    private val imagesId= arrayListOf(
+    private lateinit var mqttHandler: MqttHandler
+    private val clientId = MqttClient.generateClientId()
+
+
+    private val imagesId = arrayListOf(
         R.drawable.test1,
         R.drawable.test3,
         R.drawable.test4,
@@ -31,43 +36,44 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
-        val view=binding.root
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(view)
+        mqttHandler = MqttHandler()
+        mqttHandler.connect(SERVER_URI, clientId.toString())
 
-        binding.backButton.setOnClickListener{
+        binding.backButton.setOnClickListener {
             showPreviousImage()
 
         }
-        binding.nextButton.setOnClickListener{
+        binding.nextButton.setOnClickListener {
             showNextImage()
         }
         loadCurrentImage()
         createMqttClient()
 
         val clientId= MqttClient.generateClientId()
-        mqttClient = MqttAndroidClient(applicationContext,"@string/server_URI",clientId)
-
-
-
+         mqttClient = MqttAndroidClient(applicationContext,"@string/server_URI",clientId)
 
         lifecycleScope.launch {
             val mqtt = MqttAndroidClient(
-                this@MainActivity, "@string/server_URI", UUID.randomUUID().toString()
+                this@MainActivity, SERVER_URI, UUID.randomUUID().toString()
             )
 
-            mqtt.connect(MqttConnectOptions(),null,object:IMqttActionListener{
+            mqtt.connect(MqttConnectOptions(), null, object : IMqttActionListener {
                 override fun onSuccess(asyncActionToken: IMqttToken?) {
                     println("Connection successfull")
                     mqtt.subscribe("testtest", 1)
 
                     val clientId = MqttClient.generateClientId()
-                    mqttClient = MqttAndroidClient(applicationContext,"@string/server_URI",clientId)
+                    mqttClient =
+                        MqttAndroidClient(applicationContext, SERVER_URI, clientId)
 
                 }
 
                 override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
                     exception?.printStackTrace()
+
                 }
 
             })
@@ -80,26 +86,26 @@ class MainActivity : AppCompatActivity() {
                 override fun messageArrived(topic: String?, message: MqttMessage?) {
                     println("$topic: ${message?.payload?.toString(Charsets.UTF_8)}")
                     val msg = message?.payload?.toString(Charsets.UTF_8)
-                   runOnUiThread{
-                        when(msg){
+                    runOnUiThread {
+                        when (msg) {
                             "@string/next" -> showNextImage()
                             "@string/back" -> showPreviousImage()
-                            }
                         }
                     }
+                }
 
                 override fun deliveryComplete(token: IMqttDeliveryToken?) {
 
                 }
             })
-            val options=MqttConnectOptions().apply {
-                isAutomaticReconnect=true
+            val options = MqttConnectOptions().apply {
+                isAutomaticReconnect = true
                 isCleanSession = true
             }
             try {
-                mqttClient.connect(options,null,object :IMqttActionListener{
+                mqttClient.connect(options, null, object : IMqttActionListener {
                     override fun onSuccess(asyncActionToken: IMqttToken?) {
-                        mqttClient.subscribe("testtest",0)
+                        mqttClient.subscribe("testtest", 0)
                     }
 
                     override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
@@ -107,8 +113,7 @@ class MainActivity : AppCompatActivity() {
                     }
 
                 })
-            }
-            catch (e:MqttException){
+            } catch (e: MqttException) {
                 e.printStackTrace()
             }
         }
@@ -118,15 +123,30 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    protected override fun onDestroy(){
+        mqttHandler.disconnect()
+        super.onDestroy()
+
+    }
+    
+    private fun publishMessage(topic: String?, message: MqttMessage?) {
+        Toast.makeText(this,"Publishing message" + message,Toast.LENGTH_LONG).show()
+        mqttHandler.publish(topic, message.toString())
+    }
+    private fun subscribe(topic: String?){
+        Toast.makeText(this,"Subscribing to topic",Toast.LENGTH_LONG).show()
+        mqttHandler.subscribe(topic)
+    }
+
     private fun showNextImage() {
-        if (currentIndex<imagesId.size){
+        if (currentIndex < imagesId.size) {
             currentIndex++
             loadCurrentImage()
         }
     }
 
     private fun showPreviousImage() {
-        if (currentIndex>0){
+        if (currentIndex > 0) {
             currentIndex--
             loadCurrentImage()
         }
@@ -135,4 +155,6 @@ class MainActivity : AppCompatActivity() {
     private fun loadCurrentImage() {
         binding.image.setImageResource(imagesId[currentIndex])
     }
+
+
 }
